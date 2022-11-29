eb2861bcb97bb6a33df25174364ca0a53558357a
devthiart <devthiart@gmail.com>
2022-11-29 14:18:01 -0300

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Here I list my components that will be used by more than one page.
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
